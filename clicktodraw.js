b458a1c58ee9c4f3775f36639a1e3000dc67f891
James Prentice <jamesprenticez@gmail.com>
2021-07-16 22:20:35 +1200

    click to draw

--- clicktodraw.js
+++ clicktodraw.js
@@ -1,6 +1,43 @@
 $(document).ready(function() {
-    var canvas = document.getElementById("canvas");
-    var ctx = canvas.getContext("2d");
-    ctx.fillStyle = "#FF0000";
-    ctx.fillRect(0,0,150,75);
+    var clicks = 0;
+    var lastClick = [0, 0];
+    
+    document.getElementById('canvas').addEventListener('click', drawLine, false);
+    
+    function getCursorPosition(e) {
+        var x;
+        var y;
+    
+        if (e.pageX != undefined && e.pageY != undefined) {
+            x = e.pageX;
+            y = e.pageY;
+        } else {
+            x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+            y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+        }
+        
+        return [x, y];
+    }
+    
+    function drawLine(e) {
+        context = this.getContext('2d');
+    
+        x = getCursorPosition(e)[0] - this.offsetLeft;
+        y = getCursorPosition(e)[1] - this.offsetTop;
+        
+        if (clicks != 1) {
+            clicks++;
+        } else {
+            context.beginPath();
+            context.moveTo(lastClick[0], lastClick[1]);
+            context.lineTo(x, y, 6);
+            
+            context.strokeStyle = '#000000';
+            context.stroke();
+            
+            clicks = 0;
+        }
+        
+        lastClick = [x, y];
+    };
 });
