0627029ba08a02eb2d7819d1fc56bfe4cd843907
James Prentice <jamesprenticez@gmail.com>
2021-07-23 21:19:42 +1200

    better svg

--- scripts/clicktodrawSL.js
+++ scripts/clicktodrawSL.js
@@ -1,44 +1,69 @@
-$(document).ready(function() {
-    var clicks = 0;
-    var lastClick = [0, 0];
-    
-    document.getElementById('canvas').addEventListener('click', drawLine, false);
-    
-    function getCursorPosition(e) {
-        var x;
-        var y;
-    
-        if (e.pageX != undefined && e.pageY != undefined) {
-            x = e.pageX;
-            y = e.pageY;
-        } else {
-            x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-            y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-        }
-        
-        return [x, y];
-    }
-    
-    function drawLine(e) {
-        context = this.getContext('2d');
-    
-        x = getCursorPosition(e)[0] - this.offsetLeft;
-        y = getCursorPosition(e)[1] - this.offsetTop;
-        
-        
-        if (clicks != 1) {
-            clicks++;
-        } else {
-            context.beginPath();
-            context.moveTo(lastClick[0], lastClick[1]);
-            context.lineTo(x, y, 6);
-            
-            context.strokeStyle = '#000000';
-            context.stroke();
-            
-            clicks = 0;
-        }
-        
-        lastClick = [x, y];
-    };
-});
+const dimGroups = document.getElementById('dimensionGroups');
+const canvasEle = document.getElementById('canvas');
+const context = canvasEle.getContext('2d');
+let startPosition = {x: 0, y: 0};
+let lineCoordinates = {x: 0, y: 0};
+let isDrawStart = false;
+
+const getClientOffset = (event) => {
+    const {pageX, pageY} = event.touches ? event.touches[0] : event;
+    const x = pageX - canvasEle.offsetLeft;
+    const y = pageY - canvasEle.offsetTop;
+
+    return {
+       x,
+       y
+    } 
+}
+
+const drawLine = () => {
+   context.beginPath();
+   context.moveTo(startPosition.x, startPosition.y);
+   context.lineTo(lineCoordinates.x, lineCoordinates.y);
+   context.stroke();
+}
+
+const mouseDownListener = (event) => {
+   startPosition = getClientOffset(event);
+   isDrawStart = true;
+   console.log(startPosition)
+}
+
+const mouseMoveListener = (event) => {
+  if(!isDrawStart) return;
+  lineCoordinates = getClientOffset(event);
+  
+  clearCanvas();
+  //Can you mock this?
+  drawLine();
+  //wait untill the end
+  console.log(lineCoordinates)
+  //only want to log the last number
+  logToScreen()
+}
+
+const logToScreen = () => {
+   let table = document.getElementById("dimensionGroups")
+   let row = table.insertRow(2)
+   let cellX = row.insertCell(0)
+   let cellY = row.insertCell(1)
+   cellX.innerHTML = lineCoordinates.x
+   cellY.innerHTML = lineCoordinates.y
+   console.log(cellX)
+}
+
+const mouseupListener = (event) => {
+  isDrawStart = false;
+}
+
+const clearCanvas = () => {
+   context.clearRect(0, 0, canvasEle.width, canvasEle.height);
+}
+
+canvasEle.addEventListener('mousedown', mouseDownListener);
+canvasEle.addEventListener('mousemove', mouseMoveListener);
+canvasEle.addEventListener('mouseup', mouseupListener);
+
+canvasEle.addEventListener('touchstart', mouseDownListener);
+canvasEle.addEventListener('touchmove', mouseMoveListener);
+canvasEle.addEventListener('touchend', mouseupListener);
