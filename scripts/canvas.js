f12a1fff4915ef74593ca4d9952331853f9de757
James Prentice <jamesprenticez@gmail.com>
2021-07-24 12:25:00 +1200

    Log to screen the start postion

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,8 +1,18 @@
-var canvas = document.getElementById('canvas')
-var context = canvas.getContext('2d');
+let canvas = document.getElementById('canvas')
+let context = canvas.getContext('2d');
+let table = document.getElementById('table');
 
-// resize the canvas to fill browser window dynamically
+let startPosition =  {x: 0, y: 0}
+let movingPosition =  {x: 0, y: 0}
+let endPosition = {x: 0, y: 0}
+
+let isDrawing = false;
+
+// Resize the canvas to fill browser window dynamically
 window.addEventListener('resize', resizeCanvas, false);
+
+//Main Function to Run
+resizeCanvas();
 
 function resizeCanvas() {
         canvas.width = window.innerWidth;
@@ -12,10 +22,50 @@ function resizeCanvas() {
          * Your drawings need to be inside this function otherwise they will be reset when 
          * you resize the browser window and the canvas goes will be cleared.
          */
-        drawStuff(); 
+        // drawStuff(); 
 }
-resizeCanvas();
 
-function drawStuff() {
-        // do your drawing stuff here
+//Get mouse position relative to the canvas element on the users screen
+function getCursorCoordinates (event) {
+    
+    const position = {
+        x: event.pageX,
+        y: event.pageY
+    };
+    
+    const offset = {
+        left: canvas.offsetLeft,
+        top: canvas.offsetTop
+    };
+    
+    let reference = canvas.offsetParent;
+    
+    while(reference){
+        offset.left += reference.offsetLeft;
+        offset.top += reference.offsetTop;
+        reference = reference.offsetParent;
+    }
+    
+    return { 
+        x: position.x - offset.left,
+        y: position.y - offset.top,
+    }; 
+    
 }
+
+const mouseDownListener = (event) => {
+    startPosition = getCursorCoordinates(event);
+    logToScreen(startPosition)
+}
+
+const logToScreen = (startPosition, endPosition) => {
+    let table = document.getElementById("table")
+    let row = table.insertRow(2)
+    let cellX = row.insertCell(0)
+    let cellY = row.insertCell(1)
+    cellX.innerHTML = startPosition.x
+    cellY.innerHTML = startPosition.y
+ }
+
+//Event listeners
+canvas.addEventListener('mousedown', mouseDownListener);
